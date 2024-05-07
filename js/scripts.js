90da17b89f6e84cb742fdca3d3295f95a8590004
Danilo Polesi <43972340+danilo-polesi@users.noreply.github.com>
2024-05-07 16:54:08 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -54,3 +54,21 @@ $(function () {
     });
 
 });
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
